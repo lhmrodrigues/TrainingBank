e3283ab1f07cd2bd8ee6c4964a265bd5676a6f91
lhmrodrigues <luishenrque.m.r.993@gmail.com>
2020-06-20 17:13:49 -0300

    Inclusão do Typed.JS

--- traning-bank/src/app/content/content.component.ts
+++ traning-bank/src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-content',
@@ -9,7 +10,20 @@ export class ContentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(){
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['Training banking juros amigo', 'Peça a portabilidade e venha ser Training Bankinhg hoje mesmo', 'Inovavor digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
 
+    const typed = new Typed('.typing-element', options);
+  }
 }
